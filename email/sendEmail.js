60d1149af3edcc6de4d5b7acb3417c7da34e7e97
bigjoe <bigjoe@bigjoe.bigjoe>
2025-11-20 21:16:47 -0500

    Add sendEmail module

---
+++ email/sendEmail.js
@@ -0,0 +1,28 @@
+// backend/email/sendEmail.js
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER, // from your .env
+    pass: process.env.EMAIL_PASS, // app password
+  },
+});
+
+const sendEmail = async ({ to, subject, html }) => {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    html,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log(`✅ Email sent to ${to}`);
+  } catch (err) {
+    console.error("❌ Error sending email:", err);
+  }
+};
+
+export default sendEmail;
